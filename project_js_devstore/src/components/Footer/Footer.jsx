dc45d3c7871e35c753bba63aa00ca59a1d22575b
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2023-12-28 20:13:00 +0300

    debag mobile first

--- project_js_devstore/src/components/Footer/Footer.jsx
+++ project_js_devstore/src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import "./Footer.scss";
 import { FaLocationArrow, FaMobileAlt, FaEnvelope } from 'react-icons/fa';
-import Payment from "../../assets/payments.png"
+import Payment from "../../assets/payments.png";
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+    const navigate = useNavigate()
     return <footer className='footer'>
         <div className="footer-content">
             <div className="col">
@@ -40,7 +42,7 @@ const Footer = () => {
                 <span className="text">Projectors</span>
             </div>
             <div className="col">
-                <div className="title">Pages</div>
+                <div className="title" onClick={() => {navigate("/register")}}>Log in</div>
                 <span className="text">Home</span>
                 <span className="text">About</span>
                 <span className="text">Privacy Policy</span>
